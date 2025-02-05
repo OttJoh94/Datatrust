1132ccc96e11238e31f8ffb8b8a768cc6a9530b5
OttJoh94 <olg.johansson@gmail.com>
2025-02-05 13:25:02 +0100

    add azure key vault

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using DataTrust.Data;
 using DataTrust.Models;
 using Microsoft.AspNetCore.Authentication;
@@ -11,47 +12,47 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(options =>
 {
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = "Google";
+	options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = "Google";
 })
 .AddCookie(options =>
 {
-    // When a user logs in to Google for the first time, create a local account for that user in our database.
-    options.Events.OnValidatePrincipal += async context =>
-    {
-        var serviceProvider = context.HttpContext.RequestServices;
-        using var db = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
+	// When a user logs in to Google for the first time, create a local account for that user in our database.
+	options.Events.OnValidatePrincipal += async context =>
+	{
+		var serviceProvider = context.HttpContext.RequestServices;
+		using var db = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
 
-        string subject = context.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
-        string issuer = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Issuer;
-        string name = context.Principal.FindFirst(ClaimTypes.Name).Value;
+		string subject = context.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
+		string issuer = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Issuer;
+		string name = context.Principal.FindFirst(ClaimTypes.Name).Value;
 
-        var account = db.Accounts
-            .FirstOrDefault(p => p.OpenIDIssuer == issuer && p.OpenIDSubject == subject);
+		var account = db.Accounts
+			.FirstOrDefault(p => p.OpenIDIssuer == issuer && p.OpenIDSubject == subject);
 
-        if (account == null)
-        {
-            account = new Account
-            {
-                OpenIDIssuer = issuer,
-                OpenIDSubject = subject,
-                Name = name
-            };
-            db.Accounts.Add(account);
-        }
-        else
-        {
-            // If the account already exists, just update the name in case it has changed.
-            account.Name = name;
-        }
+		if (account == null)
+		{
+			account = new Account
+			{
+				OpenIDIssuer = issuer,
+				OpenIDSubject = subject,
+				Name = name
+			};
+			db.Accounts.Add(account);
+		}
+		else
+		{
+			// If the account already exists, just update the name in case it has changed.
+			account.Name = name;
+		}
 
-        await db.SaveChangesAsync();
-    };
+		await db.SaveChangesAsync();
+	};
 })
 .AddOpenIdConnect("Google", options =>
 {
-    options.Authority = "https://accounts.google.com";
-    /*
+	options.Authority = "https://accounts.google.com";
+	/*
     These two values (client ID and client secret) must be created in the Google Cloud Platform Console:
     https://support.google.com/cloud/answer/6158849?hl=en
     https://developers.google.com/identity/openid-connect/openid-connect
@@ -65,29 +66,32 @@ builder.Services.AddAuthentication(options =>
        }
     }
     */
-    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-    options.ResponseType = OpenIdConnectResponseType.Code;
-    options.CallbackPath = "/signin-oidc-google";
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-    options.SaveTokens = true;
-    options.GetClaimsFromUserInfoEndpoint = true;
+	options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+	options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+	options.ResponseType = OpenIdConnectResponseType.Code;
+	options.CallbackPath = "/signin-oidc-google";
+	options.Scope.Add("openid");
+	options.Scope.Add("profile");
+	options.Scope.Add("email");
+	options.SaveTokens = true;
+	options.GetClaimsFromUserInfoEndpoint = true;
 
-    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
-    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+	options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
+	options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
 });
 
 builder.Services.AddAuthorization(options =>
 {
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
+	options.FallbackPolicy = new AuthorizationPolicyBuilder()
+		.RequireAuthenticatedUser()
+		.Build();
 });
 
+var newtonUri = new Uri("https://datatrustottokv.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(newtonUri, new DefaultAzureCredential());
+
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -100,14 +104,14 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
 }
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -122,9 +126,9 @@ app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
-    SampleData.Create(context);
+	var services = scope.ServiceProvider;
+	var context = services.GetRequiredService<AppDbContext>();
+	SampleData.Create(context);
 }
 
 app.Run();
